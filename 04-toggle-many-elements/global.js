70c1eb191970222fbf63716e0578bfac4d09b7b4
TurkinsDP1-4 <antonturkin777@gmail.com>
2021-06-08 17:36:03 +0300

    4.uzd

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,30 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function()
+{
+    const button = document.querySelector('#button');
+    var isHidden = true;
+
+    button.addEventListener('click', (e) => 
+    {
+        if(isHidden)
+        {
+            let i = 0;
+            while(i < 2)
+            {
+                document.getElementsByClassName('toggle_me')[i].style.display = "block";
+                i++;
+            }
+            isHidden = !isHidden;
+        }
+        else
+        {
+            let i = 0;
+            while(i < 2)
+            {
+                document.getElementsByClassName('toggle_me')[i].style.display = "none";
+                i++;
+            }
+            isHidden = !isHidden;
+        }
+    });
+});
