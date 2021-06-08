f88c0f6285adeca50e37d1e474db75d88e5f42a6
TurkinsDP1-4 <antonturkin777@gmail.com>
2021-06-08 17:39:23 +0300

    5.uzd

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,31 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function()
+{
+    const button = document.querySelector('#toggle_button');
+    var isHidden = true;
+
+    button.addEventListener('click', (e) => 
+    {
+        console.log();
+        if(isHidden)
+        {
+            let i = 0;
+            while(i < 5)
+            {
+                document.getElementsByClassName('second_five')[0].children[i].style.display = "block"
+                i++;
+            }
+            isHidden = !isHidden;
+        }
+        else
+        {
+            let i = 0;
+            while(i < 5)
+            {
+                document.getElementsByClassName('second_five')[0].children[i].style.display = "none"
+                i++;
+            }
+            isHidden = !isHidden;
+        }
+    });
+});
