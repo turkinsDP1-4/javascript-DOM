7aac75219a5c558a95215ee026a8350a0d4ed826
TurkinsDP1-4 <antonturkin777@gmail.com>
2021-06-08 17:47:31 +0300

    6.uzd

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,10 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    var textbox = document.getElementById('full_name')
+
+    textbox.addEventListener("input", function(e){
+        console.log(textbox.textContent)
+        var paragraph = document.getElementById("greeting")
+        paragraph.innerHTML = "Hello, " + textbox.value + "!"
+    })
+})
